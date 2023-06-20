5cbe5f2dff57670c436f9333950e237fa85c658b
yourrra <yuri15616@gmailcom>
2023-06-20 17:36:35 +0300

    feat: added component Typography

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,5 @@
 import { Button } from '../components/Button'
-import { Checkbox } from '../components/Checkbox/Checkbox'
+import { Checkbox } from '../components/Checkbox'
 
 export function Main() {
   return (
